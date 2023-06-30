bd0bead20a23181a1f469c9c8d4d187bc67cd9c1
trinadh Ravuri <trinadh.ravz@gmail.com>
2023-06-30 16:24:57 +0530

    all skills and all projects added

--- mypage/urls.py
+++ mypage/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('base',views.base,name='base'),
     path('allskills',views.allskills,name='allskills'),
     path('allprojects',views.allprojects,name='allprojects'),
+    path('home',views.home,name='home'),
 ]
--- mypage/views.py
+++ mypage/views.py
@@ -5,6 +5,17 @@ from .models import Skills,Projects
 def base(request):
     return render(request,'base.html')
 
+def home(request):
+    skills = Skills.objects.all()
+    skills = skills[:4]
+    projects = Projects.objects.all()
+    projects = projects[:4]
+    context = {
+        'skills':skills,
+        'projects':projects
+    }
+    return render(request,'home.html',context)
+
 def allskills(request):
     skills = Skills.objects.all()
     context= {
